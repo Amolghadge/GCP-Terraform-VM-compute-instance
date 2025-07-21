1cf9f6600c5c571e10e41d7a48a65575998e00d7
Amol Ghadge <64397760+Amolghadge@users.noreply.github.com>
2025-07-21 23:12:53 +0530

    Update output.tf

--- modules/gcp-vm/output.tf
+++ modules/gcp-vm/output.tf
@@ -2,20 +2,20 @@
 
 output "id" {
   description = "The unique ID of the instance."
-  value       = google_compute_instance.this.id
+  value       = google_compute_instance.vm_instance.id
 }
 
 output "name" {
   description = "The name of the instance."
-  value       = google_compute_instance.this.name
+  value       = google_compute_instance.vm_instance.name
 }
 
 output "external_ip" {
   description = "The external IP address of the instance."
-  value       = google_compute_instance.this.network_interface[0].access_config[0].nat_ip
+  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
 
 output "internal_ip" {
   description = "The internal IP address of the instance."
-  value       = google_compute_instance.this.network_interface[0].network_ip
+  value       = google_compute_instance.vm_instance.network_interface[0].network_ip
 }
